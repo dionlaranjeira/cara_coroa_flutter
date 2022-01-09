dd2ca354627ca96e9491aadaa36b97c56bb21850
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2022-01-09 13:30:36 -0400

    Interface Home

---
+++ lib/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  
+  void _exibirResultado(){
+    
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff61bd68),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset("images/logo.png"),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset("images/botao_jogar.png"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
